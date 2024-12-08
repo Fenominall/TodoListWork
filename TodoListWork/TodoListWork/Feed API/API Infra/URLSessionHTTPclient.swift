4514a9e87577fdbf6b7902599710f7417f871f85
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-08 15:13:22 +0300

    Implemented `get` method in `URLSessionHTTPClient` to perform HTTP GET requests. The method handles errors, validates response data and type, and ensures compliance with the `HTTPClient` protocol using `Result` type.

--- TodoListWork/TodoListWork/Feed API/API Infra/URLSessionHTTPclient.swift
+++ TodoListWork/TodoListWork/Feed API/API Infra/URLSessionHTTPclient.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public final class  URLSessionHTTPclient: HTTPClient {
+public final class URLSessionHTTPclient: HTTPClient {
     // MARK: - Properties
     public let session: URLSession
     
@@ -16,8 +16,20 @@ public final class  URLSessionHTTPclient: HTTPClient {
         self.session = session
     }
     
+    private struct UnexpectedValuesRepresentation: Error {}
+    
     // MARK: - Methods
     public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-        
+        session.dataTask(with: url) { data, response, error in
+            completion(Result {
+                if let error = error { throw error }
+                guard let data = data,
+                      let response = response as? HTTPURLResponse else {
+                    throw UnexpectedValuesRepresentation()
+                }
+                return (data, response)
+            })
+        }
+        .resume()
     }
 }
