4d0a3a9dfa1295a751a040dc9a2ee390471f2b0e
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-08 15:27:48 +0300

    Added `TodoFeedItemsMapper` to handle mapping of network data to domain models. - Decodes JSON response into `TodoItem` objects through intermediate `RemoteTodoItem` and `Root` structs. - Validates HTTP response status code and throws an error for invalid data. - Simplifies transformation of API responses into app-specific domain entities.

---
+++ TodoListWork/TodoListWork/Feed API/TodoFeedItemsMapper.swift
@@ -0,0 +1,56 @@
+//
+//  TodoFeedItemsMapper.swift
+//  TodoListWork
+//
+//  Created by Fenominall on 12/8/24.
+//
+
+import Foundation
+
+public final class TodoFeedItemsMapper {
+    
+    private struct Root: Decodable {
+        let todos: [RemoteTodoItem]
+        
+        var feed: [TodoItem] {
+            return todos.map { $0.item }
+        }
+    }
+    
+    private struct RemoteTodoItem: Decodable {
+        let id: Int
+        let todo: String
+        let completed: Bool
+        let userId: Int
+        
+        var item: TodoItem {
+            
+            return TodoItem(
+                id: id,
+                title: todo,
+                description: nil,
+                completed: completed,
+                createdAt: Date(),
+                userID: userId
+            )
+        }
+    }
+    
+    private enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    private static let jsonDecoder = JSONDecoder()
+    
+    public static func map(
+        _ data: Data,
+        from response: HTTPURLResponse
+    ) throws -> [TodoItem] {
+        guard response.statusCode == 200 else {
+            throw Error.invalidData
+        }
+        let root = try jsonDecoder.decode(Root.self, from: data)
+        
+        return root.feed
+    }
+}
