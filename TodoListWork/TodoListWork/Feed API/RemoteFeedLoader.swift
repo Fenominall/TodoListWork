836520aabe1d0bb978a505c78112e3ff6fa7b793
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-08 14:54:07 +0300

    Added 'RemoteFeedLoader' class that conforms to the 'FeedLoader' prtocol and communicates with the 'HTTPClient' for netwrok get reqeusts

---
+++ TodoListWork/TodoListWork/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,38 @@
+//
+//  RemoteFeedLoader.swift
+//  TodoListWork
+//
+//  Created by Fenominall on 12/8/24.
+//
+
+import Foundation
+
+public final class RemoteFeedLoader: FeedLoader {
+    // MARK: - Properties
+    private let url: URL
+    private let client: HTTPClient
+    
+    public typealias Result = FeedLoader.Result
+    
+    // MARK: - Lifecycle
+    public init(
+        url: URL,
+        client: HTTPClient
+    ) {
+        self.url = url
+        self.client = client
+    }
+    
+    // MARK: - Methods
+    public func loadFeed(completion: @escaping (Result) -> Void) {
+        client.get(from: url) { [weak self] result in
+            switch result {
+                
+            case .success((_, _)):
+                break
+            case .failure(_):
+                break
+            }
+        }
+    }
+}
