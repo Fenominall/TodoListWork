4f778ca42b22073798c208f79f222793963e4de5
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-08 11:45:32 +0300

    Added new macOS framewrok for the 'TodoListWorkApp' business logic

---
+++ TodoListWork/TodoListWork/TodoListWork.h
@@ -0,0 +1,18 @@
+//
+//  TodoListWork.h
+//  TodoListWork
+//
+//  Created by Fenominall on 12/8/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TodoListWork.
+FOUNDATION_EXPORT double TodoListWorkVersionNumber;
+
+//! Project version string for TodoListWork.
+FOUNDATION_EXPORT const unsigned char TodoListWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TodoListWork/PublicHeader.h>
+
+
