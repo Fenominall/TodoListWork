03dd4f94122fb2902eca0a6c840b089d13514085
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-09 19:07:18 +0300

    Add `TodoItemDeleter` protocol to define deletion behavior for Todo items.

---
+++ TodoListWork/TodoListWork/Todo Feed Fature/TodoItemDeleter.swift
@@ -0,0 +1,14 @@
+//
+//  TodoItemDeleter.swift
+//  TodoListWork
+//
+//  Created by Fenominall on 12/9/24.
+//
+
+import Foundation
+
+public protocol TodoItemDeleter {
+    typealias Result = Swift.Result<Void, Error>
+    
+    func delete(_ item: TodoItem, completion: @escaping (Result) -> Void)
+}
