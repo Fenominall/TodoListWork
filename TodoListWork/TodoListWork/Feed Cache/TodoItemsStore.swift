2606ee0c275b0d757edf4573213c8cce316a6f78
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-09 19:47:33 +0300

    Introduce `TodoItemsStore` protocol as a Core Data boundary for Todo item persistence
    - Added `retrieve(completion:)` for fetching all stored Todo items.
    - Added `insert(_:completion:)` to insert a single `LocalTodoItem`.
    - Added `insert(_:completion:)` to insert multiple `LocalTodoItem` objects.
    - Added `update(_:completion:)` to update an existing `LocalTodoItem`.
    - Added `delete(_:completion:)` to remove a specific `LocalTodoItem`.
    
    - All methods use asynchronous completion handlers with `Swift.Result` types to handle success and failure scenarios.
    - Defines a clean boundary between business logic and Core Data infrastructure, enabling decoupled and testable architecture.
    - Provides flexibility for switching or modifying the persistence mechanism without affecting higher layers of the app.

---
+++ TodoListWork/TodoListWork/Feed Cache/TodoItemsStore.swift
@@ -0,0 +1,29 @@
+//
+//  TodoItemsStore.swift
+//  TodoListWork
+//
+//  Created by Fenominall on 12/9/24.
+//
+
+import Foundation
+
+
+public protocol TodoItemsStore {
+    typealias DeletionResult = Swift.Result<Void, Error>
+    typealias DeletionCompletion = (DeletionResult) -> Void
+    
+    typealias InsertionResult = Swift.Result<Void, Error>
+    typealias InsertionCompletion = (InsertionResult) -> Void
+    
+    typealias RetrievalResult = Result<[LocalTodoItem]?, Error>
+    typealias RetrievalCompletion = (RetrievalResult) -> Void
+    
+    typealias UpdatingResult = Swift.Result<Void, Error>
+    typealias UpdatingCompletion = (UpdatingResult) -> Void
+    
+    func retrieve(completion: @escaping RetrievalCompletion)
+    func insert(_ tasks: [LocalTodoItem], completion: @escaping InsertionCompletion)
+    func insert(_ task: LocalTodoItem, completion: @escaping InsertionCompletion)
+    func update(_ task: LocalTodoItem, completion: @escaping UpdatingCompletion)
+    func delete(_ task: LocalTodoItem, completion: @escaping DeletionCompletion)
+}
