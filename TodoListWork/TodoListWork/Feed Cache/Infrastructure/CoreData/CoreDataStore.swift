f8058e46c136ef272e9971402d95a1dbb34a9ac5
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-09 22:00:14 +0300

    Added new type 'CoreDataStore' that will act as CoreDataStack

---
+++ TodoListWork/TodoListWork/Feed Cache/Infrastructure/CoreData/CoreDataStore.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataStore.swift
+//  TodoListWork
+//
+//  Created by Fenominall on 12/9/24.
+//
+
+import Foundation
+import CoreData
+
+public final class CoreDataFeedStore {
+    
+}
