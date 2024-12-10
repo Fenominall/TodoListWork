737f9d89f5f1f9b1325b73be89c3c3adc627a2d9
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-10 22:46:52 +0300

    Added TodoItemsLoadingView protocol that will be updating the loading state for the View

--- TodoListWork/TodoListWorkiOS/Feed Feature UI/Presenter/TodoItemsLoadingView.swift
+++ TodoListWork/TodoListWorkiOS/Feed Feature UI/Presenter/TodoItemsLoadingView.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+public protocol TodoItemsLoadingView {
+    func display(_ viewModel: TodoItemsLoadingViewModel)
+}
--- TodoListWork/TodoListWorkiOS/Feed Feature UI/Presenter/TodoItemsLoadingViewModel.swift
+++ TodoListWork/TodoListWorkiOS/Feed Feature UI/Presenter/TodoItemsLoadingViewModel.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+public struct TodoItemsLoadingViewModel {
+    public let isLoading: Bool
+}
