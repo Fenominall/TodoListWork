6c6df91a2cda3acd1a71f23b2ddfa52f89177056
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-10 22:55:53 +0300

    Introduced TodoItemFeedViewModel as DTO object to be used in the VIPER implementation and work with the View

---
+++ TodoListWork/TodoListWorkiOS/Feed Feature UI/Entity/TodoItemViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  TodoItemViewModel.swift
+//  TodoListWorkiOS
+//
+//  Created by Fenominall on 12/10/24.
+//
+
+import Foundation
+
+public struct TodoItemFeedViewModel {
+    public let id: UUID
+    public let title: String
+    public let description: String?
+    public let completed: Bool
+    public let createdAt: Date
+    public let userId: Int
+    
+    public init(
+        id: UUID,
+        title: String,
+        description: String?,
+        completed: Bool,
+        createdAt: Date,
+        userId: Int
+    ) {
+        self.id = id
+        self.title = title
+        self.description = description
+        self.completed = completed
+        self.createdAt = createdAt
+        self.userId = userId
+    }
+}
