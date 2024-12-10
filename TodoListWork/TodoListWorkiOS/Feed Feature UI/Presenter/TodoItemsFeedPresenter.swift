1d270d143e54e6c7845ba4724969b12a5c00de42
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-10 22:52:17 +0300

    Add `TodoItemsFeedPresenter` to coordinate UI updates and interactor operations
    - Implement `TodoItemsFeedPresenter` to bridge the UI and interactor in the VIPER architecture.
    - Handle view lifecycle events (`viewDidLoad`) to load feed data via the interactor.
    - Delegate user actions for updating and deleting Todo items to the interactor.
    - Conform to `TodoItemsFeedLoadingInteractorOutput` to manage loading states and display fetched data or errors.
    - Conform to `TodoItemsOperationInteractorOutput` to handle post-operation updates and errors.

--- TodoListWork/TodoListWorkiOS/Feed Feature UI/Presenter/TodoItemsFeedPresenter.swift
+++ TodoListWork/TodoListWorkiOS/Feed Feature UI/Presenter/TodoItemsFeedPresenter.swift
@@ -6,3 +6,65 @@
 //
 
 import Foundation
+import TodoListWork
+
+public final class TodoItemsFeedPresenter {
+    private let view: TodoItemsFeedView
+    private let errorView: TodoItemsErrorView
+    private let loadingView: TodoItemsLoadingView
+    private let interactor: TodoItemsFeedInteractorInput
+    
+    public init(
+        view: TodoItemsFeedView,
+        errorView: TodoItemsErrorView,
+        loadingView: TodoItemsLoadingView,
+        interactor: TodoItemsFeedInteractorInput
+    ) {
+        self.view = view
+        self.errorView = errorView
+        self.loadingView = loadingView
+        self.interactor = interactor
+    }
+}
+
+extension TodoItemsFeedPresenter {
+    public func viewDidLoad() {
+        interactor.loadFeed()
+    }
+    
+    public func didRequestTodoItemDeletion(_ item: TodoItem) {
+        interactor.deleteTodoItem(item)
+    }
+    
+    public func didRequestTodoItemUpdate(_ item: TodoItem) {
+        interactor.updateTodoItem(item)
+    }
+}
+
+extension TodoItemsFeedPresenter: TodoItemsFeedLoadingInteractorOutput {
+    public func didStartLoading() {
+        errorView.display(.noError)
+        loadingView.display(TodoItemsLoadingViewModel(isLoading: true))
+    }
+    
+    public func didFinishLoading(with items: [TodoListWork.TodoItem]) {
+        loadingView.display(TodoItemsLoadingViewModel(isLoading: false))
+        errorView.display(.noError)
+        view.displayTasks(items)
+    }
+    
+    public func didFinishLoading(with error: any Error) {
+        loadingView.display(TodoItemsLoadingViewModel(isLoading: false))
+        errorView.display(.error(message: error.localizedDescription))
+    }
+}
+
+extension TodoItemsFeedPresenter: TodoItemsOperationInteractorOutput {
+    public func didFinishOperation() {
+        interactor.loadFeed()
+    }
+    
+    public func didFinishOperation(with error: any Error) {
+        errorView.display(.error(message: error.localizedDescription))
+    }
+}
