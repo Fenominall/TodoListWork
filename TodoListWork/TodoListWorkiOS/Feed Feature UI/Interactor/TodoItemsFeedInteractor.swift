641be731229e351d6dd5ba852828d1505dd1a2b3
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-10 17:44:25 +0300

    Added necessary dependencies to the 'TodoItemsFeedInteractor' to perorm CRUD operations with CoreData and data loading from the external API

--- TodoListWork/TodoListWorkiOS/Feed Feature UI/Interactor/TodoItemsFeedInteractor.swift
+++ TodoListWork/TodoListWorkiOS/Feed Feature UI/Interactor/TodoItemsFeedInteractor.swift
@@ -5,8 +5,24 @@
 //  Created by Fenominall on 12/10/24.
 //
 
-import Foundation
+import TodoListWork
 
 public final class TodoItemsFeedInteractor {
+    private let feedLoader: TodoItemsFeedLoader
+    private let feedSaver: TodoItemsFeedCache
+    private let todoUpdater: TodoItemSaver
+    private let todoDeleter: TodoItemDeleter
+    private weak var presenter: TodoItemsFeedInteractorOutput?
     
+    init(
+        feedLoader: TodoItemsFeedLoader,
+        feedSaver: TodoItemsFeedCache,
+        todoUpdater: TodoItemSaver,
+        todoDeleter: TodoItemDeleter
+    ) {
+        self.feedLoader = feedLoader
+        self.feedSaver = feedSaver
+        self.todoUpdater = todoUpdater
+        self.todoDeleter = todoDeleter
+    }
 }
