1dc8c31aefea27b41397eb9a7051899fefc67cac
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-10 17:38:15 +0300

    Started implementing VIPER acrchitectural desigh pattern for Feed Feature UI Added Intreactor object with the defined protocols for the needed actions

---
+++ TodoListWork/TodoListWorkiOS/Feed Feature UI/Interactor/TodoItemsFeedInteractor.swift
@@ -0,0 +1,12 @@
+//
+//  TodoItemsFeedInteractor.swift
+//  TodoListWorkiOS
+//
+//  Created by Fenominall on 12/10/24.
+//
+
+import Foundation
+
+public final class TodoItemsFeedInteractor {
+    
+}
---
+++ TodoListWork/TodoListWorkiOS/Feed Feature UI/Interactor/TodoItemsFeedInteractorInput.swift
@@ -0,0 +1,14 @@
+//
+//  TodoItemsFeedInteractorInput.swift
+//  TodoListWorkiOS
+//
+//  Created by Fenominall on 12/10/24.
+//
+
+import TodoListWork
+
+public protocol TodoItemsFeedInteractorInput: AnyObject {
+    func loadTodoItemsFeed()
+    func deleteTodoItem(_ item: TodoItem)
+    func updateTodoItem(_ item: TodoItem)
+}
