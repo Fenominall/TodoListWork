7fdc663dd3a3d0075d3273326fd72f743498f0ea
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-11 13:46:45 +0300

    Implement context menu and search functionality in `TodoListViewController`
    - Add context menu with actions for editing, sharing, and deleting tasks.
    - Integrate `UISearchController` for task search with search bar customization.
    - Implement `UIContextMenuConfiguration` and menu animation for task interaction.
    - Set up search bar delegates to handle bookmark button actions and search result updates.
    - Configure `UISearchController` for background obscuring and button customization.

--- TodoListWork/TodoListWorkiOS/Feed Feature UI/View/Controllers/TodoListViewController.swift
+++ TodoListWork/TodoListWorkiOS/Feed Feature UI/View/Controllers/TodoListViewController.swift
@@ -13,6 +13,12 @@ public final class TodoListViewController: UIViewController {
     
     // MARK: - View Properties
     let footerView = TodoListFooterView()
+    private var taskActionsMenu = UIMenu()
+    
+    private lazy var searchController: UISearchController = {
+        let search = UISearchController(searchResultsController: nil)
+        return search
+    }()
     
     private let todoosTableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .plain)
@@ -29,10 +35,26 @@ public final class TodoListViewController: UIViewController {
         setupUI()
         setDelegates()
         setupConstraints()
+        configureSearchController()
+        setupTodoTaskMenu()
+        
+        todoosTableView.register(TodoTaskTableViewCell.self, forCellReuseIdentifier: "TodoTaskTableViewCell")
                 
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.backButtonAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.systemYellow]
+    }
+    
+    private func setupTodoTaskMenu() {
+        let edit = UIAction(title: "Редатировать", image: AppImages.squareAndPencil.image) { _ in
+            
+        }
+        let share = UIAction(title: "Поделиться", image: AppImages.squareAndArrowUp.image) { _ in
+            
+        }
+        let delete = UIAction(title: "Удалить", image: AppImages.trash.image, attributes: .destructive) { _ in }
+        
+        taskActionsMenu = UIMenu(title: "", children: [edit, share, delete])
     }
 }
 
@@ -69,7 +91,6 @@ extension TodoListViewController {
     }
 }
 
-
 // MARK: - UITableViewDataSource
 extension TodoListViewController: UITableViewDataSource {
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -85,6 +106,80 @@ extension TodoListViewController: UITableViewDataSource {
 extension TodoListViewController: UITableViewDelegate {
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    public func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        guard let cell = tableView.cellForRow(at: indexPath) as? TodoTaskTableViewCell else {
+            return nil
+        }
+        
+        // Hide the checkmark button when the menu is active
+        UIView.animate(withDuration: 0.2) {
+            cell.isMenuActive = true
+        }
+        
+        return UIContextMenuConfiguration(identifier: indexPath as NSIndexPath, previewProvider: nil) { _ in
+            self.taskActionsMenu
+        }
+    }
+    
+    public func tableView(_ tableView: UITableView, willEndContextMenuInteraction configuration: UIContextMenuConfiguration, animator: (any UIContextMenuInteractionAnimating)?) {
+        guard let indexPath = configuration.identifier as? IndexPath,
+              let cell = tableView.cellForRow(at: indexPath) as? TodoTaskTableViewCell else {
+            return
+        }
+        
+        UIView.animate(withDuration: 0.2) {
+            cell.isMenuActive = false
+        }
+    }
+}
+
+// MARK: - UISearchBarDelegate
+extension TodoListViewController {
+    private func configureSearchController() {
+        definesPresentationContext = true
+        searchController.searchBar.searchBarStyle = .minimal
+        searchController.searchBar.placeholder = "Search"
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.delegate = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.showsBookmarkButton = true
+        
+        searchController.searchBar
+            .setImage(
+                AppImages.microphoneFill.image,
+                for: .bookmark,
+                state: .normal
+            )
+        
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+}
+
+extension TodoListViewController: UISearchBarDelegate {
+    public func searchBarBookmarkButtonClicked(_ searchBar: UISearchBar) {
+        // TODO
+    }
+}
+
+// MARK: - UISearchResultsUpdating
+extension TodoListViewController: UISearchResultsUpdating {
+    public func updateSearchResults(for searchController: UISearchController) {
+        todoosTableView.reloadData()
+    }
+}
+
+// MARK: - UISearchControllerDelegate
+extension TodoListViewController: UISearchControllerDelegate {
+    public func didPresentSearchController(_ searchController: UISearchController) {
+        // TODO
+    }
+    
+    public func didDismissSearchController(_ searchController: UISearchController) {
+        // TODO
     }
 }
 
