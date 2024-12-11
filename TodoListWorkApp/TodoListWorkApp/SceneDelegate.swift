04b05c24c548f0c937abbff2ab6976bea03c9c41
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-11 21:39:37 +0300

    Remove StoryBoard from the TodoListWorkApp, loading TodoListViewController Programmatically

--- TodoListWorkApp/TodoListWorkApp/SceneDelegate.swift
+++ TodoListWorkApp/TodoListWorkApp/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import TodoListWorkiOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -13,10 +14,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // Set global appearance for UINavigationBar
+        let appearance = UINavigationBar.appearance()
+        appearance.tintColor = .systemYellow // Sets back button and other bar item colors
+        
+        let window = UIWindow(windowScene: windowScene)
+        let viewController = TodoListViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
