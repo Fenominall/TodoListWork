ef4463301bf3a381b3a8810506e66f4ee6f3778c
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-11 12:04:41 +0300

    Implement TodoListViewController with table view and footer view
    - Added `TodoListViewController` as the main view controller for displaying tasks.
    - Integrated `TodoListFooterView` with SnapKit constraints for layout.
    - Set up a `UITableView` for task items with delegate and data source methods.
    - Implemented `TodoListFooterViewDelegate` to handle footer actions.
    - Configured navigation bar appearance with large titles and custom back button style.

--- TodoListWork/TodoListWorkiOS/Feed Feature UI/View/Controllers/TodoListViewController.swift
+++ TodoListWork/TodoListWorkiOS/Feed Feature UI/View/Controllers/TodoListViewController.swift
@@ -9,8 +9,88 @@ import UIKit
 import SnapKit
 
 public final class TodoListViewController: UIViewController {
+    // MARK: - Properties
     
+    // MARK: - View Properties
+    let footerView = TodoListFooterView()
+    
+    private let todoosTableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.backgroundColor = .systemBackground
+        tableView.separatorStyle = .none
+        tableView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    // MARK: - Lifecycle
     public override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
+        setDelegates()
+        setupConstraints()
+                
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backButtonAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.systemYellow]
+    }
+}
+
+// MARK: - Helpers
+extension TodoListViewController {
+    private func setupUI() {
+        title = "Задачи"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func setDelegates() {
+        todoosTableView.delegate = self
+        todoosTableView.dataSource = self
+        footerView.delegate = self
+    }
+    
+    private func setupConstraints() {
+        view.addSubview(todoosTableView)
+        view.addSubview(footerView)
+        
+        footerView.snp.makeConstraints {
+            $0.bottom.leading.trailing.equalToSuperview()
+            $0.height.equalTo(83)
+        }
+        
+        todoosTableView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(footerView.snp.top)
+        }
+        
+        // Bring the footer view to the front
+        view.bringSubviewToFront(footerView)
+    }
+}
+
+
+// MARK: - UITableViewDataSource
+extension TodoListViewController: UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension TodoListViewController: UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+// MARK: - TodoListFooterViewDelegate
+extension TodoListViewController: TodoListFooterViewDelegate {
+    func didTapAddNewTaskButton() {
+        // TODO
     }
 }
