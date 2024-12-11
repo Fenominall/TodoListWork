a0e851fa4394879db3074273891202ac792f6a7b
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-11 11:40:17 +0300

    Added TodoListViewController for the Feed Screen

---
+++ TodoListWork/TodoListWorkiOS/Feed Feature UI/View/Controllers/TodoListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TodoListViewController.swift
+//  TodoListWorkiOS
+//
+//  Created by Fenominall on 12/11/24.
+//
+
+import UIKit
+import SnapKit
+
+public final class TodoListViewController: UIViewController {
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
