d9ee6772ba491248242f1cb12cf25b63e940fd48
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-12 13:35:32 +0300

    Add WeakRefVirtualProxy to decouple strong references for TodoItemsLoadingView and TodoItemsErrorView This avoids retaining strong references, which can help prevent retain cycles and memory leaks

---
+++ TodoListWorkApp/TodoListWorkApp/Shared Helpers/WeakRefVirtualproxy.swift
@@ -0,0 +1,29 @@
+//
+//  WeakRefVirtualproxy.swift
+//  TodoListWorkApp
+//
+//  Created by Fenominall on 12/12/24.
+//
+
+import Foundation
+import TodoListWorkiOS
+
+final class WeakRefVirtualproxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualproxy: TodoItemsLoadingView where T: TodoItemsLoadingView {
+    func display(_ viewModel: TodoListWorkiOS.TodoItemsLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualproxy: TodoItemsErrorView where T: TodoItemsErrorView {
+    func display(_ viewModel: TodoListWorkiOS.TodoItemsErrorViewModel) {
+        object?.display(viewModel)
+    }
+}
