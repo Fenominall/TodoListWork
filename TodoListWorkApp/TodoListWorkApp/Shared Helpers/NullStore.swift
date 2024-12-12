fda4e6e62f1ee93fc213621e15fbec4673f2add8
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-12 15:40:09 +0300

    Added NullStore to return empty success if loading of CoreDataStore fails, preveting an app crash

---
+++ TodoListWorkApp/TodoListWorkApp/Shared Helpers/NullStore.swift
@@ -0,0 +1,31 @@
+//
+//  NullStore.swift
+//  TodoListWorkApp
+//
+//  Created by Fenominall on 12/12/24.
+//
+
+import Foundation
+import TodoListWork
+
+final class NullStore: TodoItemsStore {
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.success((.none)))
+    }
+    
+    func insert(_ tasks: [TodoListWork.LocalTodoItem], completion: @escaping InsertionCompletion) {
+        completion(.success(()))
+    }
+    
+    func insert(_ task: TodoListWork.LocalTodoItem, completion: @escaping InsertionCompletion) {
+        completion(.success(()))
+    }
+    
+    func update(_ task: TodoListWork.LocalTodoItem, completion: @escaping UpdatingCompletion) {
+        completion(.success(()))
+    }
+    
+    func delete(_ task: TodoListWork.LocalTodoItem, completion: @escaping DeletionCompletion) {
+        completion(.success(()))
+    }
+}
