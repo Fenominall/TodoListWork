f119de727726af36a0be0602aa8948e88935695f
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-12 08:35:40 +0300

    Added view method to the 'TodoItemCelController'  that will be used by TodoListViewController to populate TodoItemtableViewCell for the safety check checling if the cell nill, and creating a cell, if the cell cannot be created with the binded method, then return an empty UITAbleViewcell Added binded method to bind data from the viewModel with the TodoItemTableViewCell creating a enw cell

--- TodoListWork/TodoListWorkiOS/Feed Feature UI/View/Controllers/TodoItemCellController.swift
+++ TodoListWork/TodoListWorkiOS/Feed Feature UI/View/Controllers/TodoItemCellController.swift
@@ -9,6 +9,30 @@ import Foundation
 import UIKit
 
 final class TodoItemCellController {
+    private(set) var viewModel: TodoItemFeedViewModel
     private var cell: TodoItemTableViewCell?
+    private(set) var onCompletedStatusToggle: (TodoItemFeedViewModel) -> Void
     
+    public init(viewModel: TodoItemFeedViewModel,
+                onCompletedStatusToggle: @escaping (TodoItemFeedViewModel) -> Void) {
+        self.viewModel = viewModel
+        self.onCompletedStatusToggle = onCompletedStatusToggle
+    }
+    
+    public func view() -> UITableViewCell {
+        if cell == nil {
+            cell = binded(TodoItemTableViewCell())
+        }
+        return cell ?? UITableViewCell()
+    }
+    
+    private func binded(_ cell: TodoItemTableViewCell) -> TodoItemTableViewCell {
+        cell.configure(
+            taskTitle: viewModel.title,
+            taskDescription: viewModel.description ?? "",
+            taskDate: dateConvertedToDMYString(date: viewModel.createdAt),
+            isCompleted: viewModel.completed
+        )
+        return cell
+    }
 }
