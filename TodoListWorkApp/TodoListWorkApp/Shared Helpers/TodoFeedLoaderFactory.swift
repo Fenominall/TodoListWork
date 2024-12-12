af600fe607e2c41c0bbc0acedb6499d73f6ba9cd
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-12 15:35:21 +0300

    Add TodoFeedLoaderFactory to load the feed from the remote  or local based on the firstLaunch Key if has launched before the feed be loaded from the locale store

--- TodoListWorkApp/TodoListWorkApp/Shared Helpers/TodoFeedLoaderFactory.swift
+++ TodoListWorkApp/TodoListWorkApp/Shared Helpers/TodoFeedLoaderFactory.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import TodoListWork
+
+final class TodoFeedLoaderFactory {
+    private let client: HTTPClient
+    private let url: URL
+    private let localStore: TodoItemsStore
+    private let firstFeedLaunchManager: FirstFeedLaunchManager
+
+    init(
+        client: HTTPClient,
+        url: URL,
+        localStore: TodoItemsStore,
+        firstFeedLaunchManager: FirstFeedLaunchManager
+    ) {
+        self.client = client
+        self.url = url
+        self.localStore = localStore
+        self.firstFeedLaunchManager = firstFeedLaunchManager
+    }
+
+    func makeRemoteFeedLoader() -> RemoteFeedLoader {
+        return RemoteFeedLoader(url: url, client: client)
+    }
+
+    func makeLocalFeedLoader() -> LocalFeedCacheManager {
+        return LocalFeedCacheManager(store: localStore)
+    }
+
+    private func makeCacheDecorator(decoratee: TodoItemsFeedLoader) -> TodoFeedLoaderCacheDecorator {
+        return TodoFeedLoaderCacheDecorator(decoratee: decoratee, cache: makeLocalFeedLoader())
+    }
+
+    /// Facade method to create the appropriate loader
+    func makeFeedLoader() -> TodoItemsFeedLoader {
+        if firstFeedLaunchManager.isFirstLaunch() {
+            return makeLocalFeedLoader()
+        } else {
+            return makeCacheDecorator(decoratee: makeRemoteFeedLoader())
+        }
+    }
+}
