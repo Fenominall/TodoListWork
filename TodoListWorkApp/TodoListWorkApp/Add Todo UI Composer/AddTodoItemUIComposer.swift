8b1e7ecd5b842da36ca4de097fce209d551f0607
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-13 11:15:01 +0300

    Add AddTodoItemUIComposer to assemble Add/Edit Todo screen components
    Implemented AddTodoItemUIComposer to compose and configure the AddEditTodoItemViewController.
    Integrated AddEditTodoNavigationRouter for navigation handling.
    Used AddEditTodoItemInteractor with a MainQueueDispatchDecorator to manage saving operations on the main thread.
    Connected AddEditTodoItemPresenter to mediate between the interactor and the view controller.

--- TodoListWorkApp/TodoListWorkApp/Add Todo UI Composer/AddTodoItemUIComposer.swift
+++ TodoListWorkApp/TodoListWorkApp/Add Todo UI Composer/AddTodoItemUIComposer.swift
@@ -5,4 +5,23 @@
 //  Created by Fenominall on 12/13/24.
 //
 
-import Foundation
+import UIKit
+import TodoListWork
+import TodoListWorkiOS
+
+final class AddTodoItemUIComposer {
+    private init() {}
+    
+    static func composedWith(todoSaver: TodoItemSaver) -> AddEditTodoItemViewController {
+        let controller = AddEditTodoItemViewController()
+        let router = AddEditTodoNavigationRouter(controller: controller)
+        
+        let interactor = AddEditTodoItemInteractor(todoSaver: MainQueueDispatchDecorator(decoratee: todoSaver))
+        
+        let presenter = AddEditTodoItemPresenter(interactor: interactor, router: router)
+        
+        interactor.presenter = presenter
+        
+        return controller
+    }
+}
