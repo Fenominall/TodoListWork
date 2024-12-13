2d75c257692e13d8363208ff03c51cae575f488b
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-13 10:49:31 +0300

    Add AddEditTodoNavigationRouter to handle navigation from Add/Edit Todo screen to feed screen

--- TodoListWork/TodoListWorkiOS/Add Edit Todo Feauture UI/Router/AddEditTodoItemRouter.swift
+++ TodoListWork/TodoListWorkiOS/Add Edit Todo Feauture UI/Router/AddEditTodoItemRouter.swift
@@ -5,8 +5,20 @@
 //  Created by Fenominall on 12/13/24.
 //
 
-import Foundation
+import UIKit
 
 public protocol AddEditTodoRouter {
     func routeToTasksFeed()
+}
+
+public final class AddEditTodoNavigationRouter: AddEditTodoRouter {
+    weak var controller: UIViewController?
+    
+    public init(controller: UIViewController? = nil) {
+        self.controller = controller
+    }
+    
+    public func routeToTasksFeed() {
+        controller?.navigationController?.popViewController(animated: true)
+    }
 }
