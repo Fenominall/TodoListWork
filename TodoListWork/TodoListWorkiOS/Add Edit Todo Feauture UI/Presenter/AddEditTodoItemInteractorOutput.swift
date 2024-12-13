86c47997f18339de1cddb600e986667f21e0eecc
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-13 10:36:15 +0300

    Added 'AddEditTodoItemInteractorOutput' to provide the interactor output to the presenter via AddEditTodoItemInteractorOutput Handeling update, save and error case

---
+++ TodoListWork/TodoListWorkiOS/Add Edit Todo Feauture UI/Presenter/AddEditTodoItemInteractorOutput.swift
@@ -0,0 +1,15 @@
+//
+//  AddEditTodoItemInteractorOutput.swift
+//  TodoListWorkiOS
+//
+//  Created by Fenominall on 12/13/24.
+//
+
+import Foundation
+import TodoListWork
+
+public protocol AddEditTodoItemInteractorOutput: AnyObject {
+    func didSaveTodo(_ todo: TodoItem)
+    func didUpdateTodo(_ todo: TodoItem)
+    func didFinishWithError(_ error: Error)
+}
