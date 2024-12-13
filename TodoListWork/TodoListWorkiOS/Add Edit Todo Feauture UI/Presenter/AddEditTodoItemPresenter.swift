0c625ba82f4350c1d62c40a0e259143b4394cc7f
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-13 10:51:43 +0300

    Provided the implementation for the AddEditTodoItemInteractorOutput in the AddEditTodoItemPresenter

--- TodoListWork/TodoListWorkiOS/Add Edit Todo Feauture UI/Presenter/AddEditTodoItemPresenter.swift
+++ TodoListWork/TodoListWorkiOS/Add Edit Todo Feauture UI/Presenter/AddEditTodoItemPresenter.swift
@@ -11,12 +11,24 @@ import TodoListWork
 public final class AddEditTodoItemPresenter {
     private let interactor: AddEditTodoItemInteractorInput
     private let router: AddEditTodoRouter
-
+    
     public init(
         interactor: AddEditTodoItemInteractorInput,
         router: AddEditTodoRouter
     ) {
         self.interactor = interactor
         self.router = router
+    }
+}
+
+extension AddEditTodoItemPresenter: AddEditTodoItemInteractorOutput {
+    public func didSaveTodo(_ todo: TodoListWork.TodoItem) {
+        interactor.save(todo)
+        router.routeToTasksFeed()
+    }
+    
+    public func didUpdateTodo(_ todo: TodoListWork.TodoItem) {
+        interactor.update(todo)
+        router.routeToTasksFeed()
     }
 }
