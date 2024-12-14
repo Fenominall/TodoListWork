da0ab93d5b6a7c56d626d23584c9f1c7fe770fbf
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-14 19:51:14 +0300

    Using AddEditTodoItemViewInput as a dependencie to the AddEdiItemPresenter

--- TodoListWork/TodoListWorkiOS/Add Edit Todo Feauture UI/Presenter/AddEditTodoItemPresenter.swift
+++ TodoListWork/TodoListWorkiOS/Add Edit Todo Feauture UI/Presenter/AddEditTodoItemPresenter.swift
@@ -11,16 +11,20 @@ import TodoListWork
 public final class AddEditTodoItemPresenter {
     private let interactor: AddEditTodoItemInteractorInput
     private let router: AddEditTodoRouter
-    
+    private let view: AddEditTodoItemViewInput
+
     public init(
         interactor: AddEditTodoItemInteractorInput,
-        router: AddEditTodoRouter
+        router: AddEditTodoRouter,
+        view: AddEditTodoItemViewInput
     ) {
         self.interactor = interactor
         self.router = router
+        self.view = view
     }
 }
 
+// MARK: - AddEditTodoItemInteractorOutput
 extension AddEditTodoItemPresenter: AddEditTodoItemInteractorOutput {
     public func didSaveTodo() {
         router.routeToTasksFeed()
