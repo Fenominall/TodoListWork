bbea17af6bc654c35972fc518059d0e4c37ab49b
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-15 13:16:11 +0300

    Refactoring the code moving the logic from the 'AddEditTodoItemViewModel' to the Presenter to adhere the VIPER architechture

--- TodoListWork/TodoListWorkiOS/Add Edit Todo Feauture UI/Presenter/AddEditTodoItemPresenter.swift
+++ TodoListWork/TodoListWorkiOS/Add Edit Todo Feauture UI/Presenter/AddEditTodoItemPresenter.swift
@@ -12,15 +12,25 @@ public final class AddEditTodoItemPresenter {
     private let interactor: AddEditTodoItemInteractorInput
     private let router: AddEditTodoRouter
     private let view: AddEditTodoItemViewInput
+    
+    private let todoToEdit: TodoItem?
+    private var currentTitle: String
+    private var currentDate: Date
+    private var currentDescription: String?
 
     public init(
         interactor: AddEditTodoItemInteractorInput,
         router: AddEditTodoRouter,
-        view: AddEditTodoItemViewInput
+        view: AddEditTodoItemViewInput,
+        todoToEdit: TodoItem?
     ) {
         self.interactor = interactor
         self.router = router
         self.view = view
+        self.todoToEdit = todoToEdit
+        self.currentTitle = todoToEdit?.title ?? ""
+        self.currentDate = todoToEdit?.createdAt ?? Date()
+        self.currentDescription = todoToEdit?.description
     }
 }
 
