b9a3c810c07d6ca43a13e9b8e673e31f9619215a
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-15 15:57:24 +0300

    Make AddEditTotoItemViewAdapter conform to the AddEditTodoItemViewInput with the updated signatue

--- TodoListWorkApp/TodoListWorkApp/Add Todo UI Composer/AddEditTotoItemViewAdapter.swift
+++ TodoListWorkApp/TodoListWorkApp/Add Todo UI Composer/AddEditTotoItemViewAdapter.swift
@@ -9,13 +9,13 @@ import Foundation
 import TodoListWorkiOS
 
 final class AddEditTotoItemViewAdapter: AddEditTodoItemViewInput {
-    func updateTodoWith(title: String, date: String, description: String?) {
-        
-    }
-    
     private weak var controller: AddEditTodoItemViewController?
     
     init(controller: AddEditTodoItemViewController) {
         self.controller = controller
+    }
+    
+    func updateTodoWith(title: String, date: String, description: String?) {
+        controller?.updateUIwith(title: title, date: date, description: description)
     }
 }
