965be8b6166748a3604b27a8d9708c2a4b3c5ffb
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-19 12:05:14 +0300

    Created ShareTodoUIComposer to compose 'ShareTodoUIActivityViewController' with all its components

--- TodoListWorkApp/TodoListWorkApp/Share Todo UI Composer/ShareTodoUIComposer.swift
+++ TodoListWorkApp/TodoListWorkApp/Share Todo UI Composer/ShareTodoUIComposer.swift
@@ -5,4 +5,16 @@
 //  Created by Fenominall on 12/19/24.
 //
 
-import Foundation
+import TodoListWork
+import TodoListWorkiOS
+
+final class ShareTodoUIComposer {
+    private init() {}
+    
+    static func composed(with item: TodoItem) -> ShareTodoUIActivityViewController {
+        let viewModel = ShareTodoItemViewModel(with: item)
+        
+        let view = ShareTodoUIActivityViewController(viewModel: viewModel)
+        return view
+    }
+}
