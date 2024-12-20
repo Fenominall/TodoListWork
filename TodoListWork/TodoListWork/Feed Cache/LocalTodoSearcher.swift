21bb3cf2d93af05450a533ddc36af841600494cf
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-20 15:48:05 +0300

    Implement search method in LocalTodoSearcher to handle search queries

--- TodoListWork/TodoListWork/Feed Cache/LocalTodoSearcher.swift
+++ TodoListWork/TodoListWork/Feed Cache/LocalTodoSearcher.swift
@@ -18,6 +18,18 @@ public final class LocalTodoSearcher: TodoSearcher {
         by query: String,
         completion: @escaping (TodoSearcher.Result) -> Void
     ) {
-        
+        store.search(query) { [weak self] result in
+            guard self != nil else { return }
+            
+            switch result {
+            case let .success(.some(searchResults)):
+                let feed = searchResults.toModels()
+                completion(.success(feed))
+            case let .failure(error):
+                completion(.failure(error))
+            case .success(.none):
+                completion(.success([]))
+            }
+        }
     }
 }
