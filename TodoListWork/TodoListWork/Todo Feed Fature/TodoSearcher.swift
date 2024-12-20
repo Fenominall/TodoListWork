08e7fb2ef7b6aca4324692aeca46e13f41422c3f
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-20 14:48:33 +0300

    Add TodoSearcher protocol for querying TodoItem data
    Ensures abstraction and flexibility for implementing search functionality in the data layer.

---
+++ TodoListWork/TodoListWork/Todo Feed Fature/TodoSearcher.swift
@@ -0,0 +1,14 @@
+//
+//  TodoSearcher.swift
+//  TodoListWork
+//
+//  Created by Fenominall on 12/20/24.
+//
+
+import Foundation
+
+public protocol TodoSearcher {
+    typealias Result = Swift.Result<[TodoItem], Error>
+    
+    func search(query: String, completion: @escaping (Result) -> Void)
+}
