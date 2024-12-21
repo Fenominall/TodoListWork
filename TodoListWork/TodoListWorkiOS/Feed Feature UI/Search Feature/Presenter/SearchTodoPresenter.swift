51ae12bcc3c4f78b486e75117fb7dc6e3f49fcff
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-21 19:33:04 +0300

    Add SearchTodoPresenter with interactor input dependency and interactor output conformance

--- TodoListWork/TodoListWorkiOS/Feed Feature UI/Search Feature/Presenter/SearchTodoPresenter.swift
+++ TodoListWork/TodoListWorkiOS/Feed Feature UI/Search Feature/Presenter/SearchTodoPresenter.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import TodoListWork
+
+public final class SearchTodoPresenter {
+    private let interactor: SearchTodoInteractorInput
+    
+    init(interactor: SearchTodoInteractorInput) {
+        self.interactor = interactor
+    }
+}
+
+extension SearchTodoPresenter: SearchTodoInteractorOutput {
+    public func didFinishSearchingTodo(with result: [TodoListWork.TodoItem]) {
+        
+    }
+    
+    public func didFinishSearchingTodo(with error: any Error) {
+        
+    }
+}
