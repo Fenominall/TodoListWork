fa47a66dedc316bed885907214da3ffbd188e0c7
Vladislav Todorov <it.vtodorov@gmail.com>
2024-12-23 17:36:20 +0300

    SearchTodoInteractorOutput was renamed to ResourceLoadingInteractorOutput, using for the SearchTodoInteractor

--- TodoListWork/TodoListWorkiOS/Feed Feature UI/Search Feature/Interactor/SearchTodoInteractor.swift
+++ TodoListWork/TodoListWorkiOS/Feed Feature UI/Search Feature/Interactor/SearchTodoInteractor.swift
@@ -10,21 +10,27 @@ import TodoListWork
 
 public final class SearchTodoInteractor: SearchTodoInteractorInput {
     private let store: TodoSearcher
-    public weak var presenter: SearchTodoInteractorOutput?
-    
-    init(store: TodoSearcher) {
+    public weak var presenter: ResourceLoadingInteractorOutput?
+
+    public init(
+        store: TodoSearcher,
+        presenter: ResourceLoadingInteractorOutput? = nil
+    ) {
         self.store = store
+        self.presenter = presenter
     }
     
     public func searchTodo(query: String) {
+        presenter?.didStartLoading()
+        
         store.search(by: query) { [weak self] result in
             
             switch result {
             case let .success(items):
-                self?.presenter?.didFinishSearching(with: items)
+                self?.presenter?.didFinishLoading(with: items)
                 
             case let .failure(error):
-                self?.presenter?.didFinishSearching(with: error)
+                self?.presenter?.didFinishLoading(with: error)
             }
         }
     }
